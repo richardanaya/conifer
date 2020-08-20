8c3c298aef26c652572577602c72cf66bd71abb6
richard <richard>
2020-08-20 01:30:53 -0400

    fix

--- examples/mouse_test.rs
+++ examples/mouse_test.rs
@@ -3,15 +3,33 @@ use std::io::Read;
 fn main() {
     let device = OpenOptions::new()
         .read(true)
-        .open("/dev/input/mouse2")
+        .open("/dev/input/mouse0")
         .unwrap();
-
+    let mut x = 0 as i32;
+    let mut y = 0 as i32;
+    let mut down = false;
+    let mut first_down = false;
     loop {
         let mut b = (&device).take(3);
 
-        let mut buffer = [0;3];
+        let mut buffer = [0; 3];
         b.read(&mut buffer);
-
-        println!("{:?}", buffer);
+        let c = buffer[0];
+        if c == 8 {
+           down = false;
+        }
+        else if c == 9 || c == 25 || c == 41 || c == 57 {
+            if down == false {
+                println!("{} {}",buffer[1],buffer[2]);
+                x = buffer[1] as i32;
+                y = buffer[2] as i32;
+            } else {
+                x += buffer[1] as i8 as i32;
+                y -= buffer[2] as i8 as i32;
+                //println!("{} {}", x, y);
+            }
+        } else {
+            println!("{:?}", buffer);
+        }
     }
 }

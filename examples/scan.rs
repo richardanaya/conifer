e0e156e330202e791bac86f868847bb33b2178c0
Richard <Richard>
2020-08-26 08:03:41 -0700

    get file path

--- examples/scan.rs
+++ examples/scan.rs
@@ -1,4 +1,7 @@
 use evdev::*;
+use std::fs::File;
+use std::os::unix::io::FromRawFd;
+
 
 fn main() {
     let dev = evdev::enumerate();
@@ -8,6 +11,8 @@ fn main() {
                 let abs = 1 << idx;
                 if (d.absolute_axes_supported().bits() & abs) == 1 {
                     println!("name: {}",d.name().clone().into_string().unwrap());
+                    let file = unsafe { File::from_raw_fd(d.fd()) };
+                    println!("file: {:#?}",file);
                    println!("abs: {:#?}", &d.state().abs_vals[idx as usize]);
                 }
             }

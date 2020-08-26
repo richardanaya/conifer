396f91adde5681a91ae42d64ad3bd7d5d8d1e52f
Richard <Richard>
2020-08-26 07:59:08 -0700

    look for absolute

--- examples/scan.rs
+++ examples/scan.rs
@@ -1,4 +1,16 @@
+use evdev::*;
+
 fn main() {
     let dev = evdev::enumerate();
-    println!("{:#?}",dev);
+    for d in dev.iter() {
+        if d.events_supported().contains(ABSOLUTE) {
+            for idx in 0..0x3f {
+                let abs = 1 << idx;
+                if (d.absolute_axes_supported().bits() & abs) == 1 {
+                    println!("name: {}",d.name().clone().into_string().unwrap());
+                   println!("abs: {:#?}", &d.state().abs_vals[idx as usize]);
+                }
+            }
+        }
+    }
 }

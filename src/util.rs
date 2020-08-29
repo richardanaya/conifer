f535981213100ec0ad13bed5c8b77b676334f2d8
Richard <Richard>
2020-08-29 16:30:50 -0700

    adding random_n

--- src/util.rs
+++ src/util.rs
@@ -3,3 +3,10 @@ use rand::Rng;
 pub fn random() -> f32 {
     rand::thread_rng().gen::<f32>()
 }
+
+pub fn random_n<T>(n: T) -> T
+where
+    T: Into<f32> + From<f32>,
+{
+    f32::floor(n.into() * random()).into()
+}

57f370966d51acb98580d20e666b37880671f270
Richard <Richard>
2020-08-30 13:58:28 -0700

    better explanation

--- examples/transparent_image.rs
+++ examples/transparent_image.rs
@@ -3,8 +3,8 @@ use conifer::prelude::*;
 fn main() -> Result<(), Box<dyn Error>> {
     let img_pine = load_image("examples/images/pine.png")?;
 
-    // create a dithered blit mask
-    let mut dither = img_pine.create_blitmap_from_alpha();
+    // create a blit mask from any alpha > 0
+    let blit_map = img_pine.create_blitmap_from_alpha();
 
     Config::auto()?.run(move |canvas, swipe, _delta_time| {
         if swipe.is_some() {
@@ -16,7 +16,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 &img_pine,
                 (random() * canvas.width as f32) as isize - img_pine.width as isize / 2,
                 (random() * canvas.height as f32) as isize - img_pine.height as isize / 2,
-                &dither,
+                &blit_map,
             )?;
         }
         Ok(RunResponse::Draw)

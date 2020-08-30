b82155a1c2faf96f52e5a06aa02c7848e1d6e021
Richard <Richard>
2020-08-30 05:37:09 -0700

    fixing example

--- examples/draw_image.rs
+++ examples/draw_image.rs
@@ -2,13 +2,13 @@ use conifer::prelude::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let img_pine = load_image("examples/images/pine.png")?;
-    Config::auto()?.run(|canvas, swipe, _delta_time| {
+    Config::auto()?.run(move |canvas, swipe, _delta_time| {
         if let Some(s) = swipe {
             return Ok(RunResponse::Exit);
         }
         // let's draw some images randomly
         for _ in 0..100 {
-            canvas.draw_frame(
+            canvas.draw_canvas(
                 &img_pine,
                 (random() * canvas.width as f32) as isize - img_pine.width as isize / 2,
                 (random() * canvas.width as f32) as isize - img_pine.height as isize / 2,
